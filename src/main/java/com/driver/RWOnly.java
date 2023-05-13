5608fbbf90bfd53c38c211bfebd98e467f8a0ce1
Galactus_@_3000 <86247957+Priyankkumawat@users.noreply.github.com>
2023-05-13 14:15:47 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    String name;
+    RWonly(){
+        name="";
+    }
+    void setter(String a){
+        name=a;
+    }
+    String getter(){
+        return name;
+    }
 }
