29d364c4c2599e9ea6a94094fa8af5da2b23e44b
Galactus__3000 <priyankkumawat7223@gmail.com>
2023-05-18 11:49:40 +0530

    v2

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    void setName(String a){
+    public void setName(String a){
         name=a;
     }
-    String getName(){
+    public String getName(){
         return name;
     }
 }
